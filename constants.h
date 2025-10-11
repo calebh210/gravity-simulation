09c3dbfd62b3d73cdd592c78da9d3ba06f0b3aa6
Caleb <caleb.harmon2345@gmail.com>
2025-10-11 21:17:09 +0100

    added relative frame 2body problem, added basic collision

--- constants.h
+++ constants.h
@@ -4,7 +4,14 @@ double G = (6.6742E-11);
 //One Astronomical Unit
 long AU = 149597870700;
 
-#define RUN_LIMIT 100000
+// number of frames to run for. -1 will be infinite
+#define RUN_LIMIT -1
 
 #define PI 3.14159265358979323846f
 
+// speed of light in m/s
+const long c = 299792458;
+
+// Celestial Masses
+double mass_earth = 5.97219E24;
+double mass_sun = 1.9891E30;
